373496550991858131a562440ff334c3304f3dd4
cheney@imichi.cn <cheney@imichi.cn>
2024-04-21 20:28:53 +0800

    更改base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
+  base: '/sqllab/',
 
   title: 'VuePress',
   description: 'My first VuePress Site',
